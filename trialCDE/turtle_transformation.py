a1f5c3289c669776edb11a84a23885e6e6bd5f67
pabloalarconm <pabloalarconmoreno@gmail.com>
2021-11-30 12:59:19 +0100

    turtle transformation tested

--- trialCDE/turtle_transformation.py
+++ trialCDE/turtle_transformation.py
@@ -8,39 +8,37 @@ from os.path import isfile, join
 
 # Gets path as arguments
 argv = sys.argv[1:]
-path = argv[0]
+
+allfiles = [f for f in listdir(argv[0]) if isfile(join(argv[0], f))]
+print(allfiles)
+
+def get_filename(file):
+    name_split = file.split(sep=".")
+    name = name_split[0]
+    filename = name + "." + "ttl"
+    print(filename)
+    return(filename)
 
 
-# allfiles = [f for f in listdir(path) if isfile(join(path, f))]
-# print(allfiles)
+for afile in allfiles:
 
+    if afile.endswith('.nt'):
 
-# def get_filename(file):
-#     name_split = file.split(sep=".")
-#     name = name_split[0]
-#     filename = name + "." + "ttl"
-#     return(filename)
+        g = Graph()
+        g.parse(str(argv[0]+ afile), format="turtle")
 
+        g.namespace_manager.bind('this', URIRef("http://example.org/data/"))
+        g.namespace_manager.bind('sio', URIRef("http://semanticscience.org/resource/"))
+        g.namespace_manager.bind('obo', URIRef("http://purl.obolibrary.org/obo/"))
+        #all_ns = [n for n in g.namespace_manager.namespaces()]
+        #print(all_ns)
 
-# for afile in allfiles:
+        name_file = get_filename(afile)
+        os.chdir(path=str(argv[0]))
+        g.serialize(destination = str(argv[0] + name_file), format="turtle")
 
-#     if afile.endswith('.nt'):
-
-#         g = Graph()
-#         g.parse(str(argv[0]+ afile), format="turtle")
-
-#         g.namespace_manager.bind('this', URIRef("http://example.org/data/"))
-#         g.namespace_manager.bind('sio', URIRef("http://semanticscience.org/resource/"))
-#         g.namespace_manager.bind('obo', URIRef("http://purl.obolibrary.org/obo/"))
-#         #all_ns = [n for n in g.namespace_manager.namespaces()]
-#         #print(all_ns)
-
-#         name_file = get_filename(afile)
-#         os.chdir(path=str(argv[0]))
-#         g.serialize(destination = str(argv[0] + name_file), format="turtle")
-
-#     else:
-#         print("File named {} it's not a .nt file so, no edition on it will be done.".format(afile))
+    else:
+        print("File named {} it's not a .nt file so, no edition on it will be done.".format(afile))
 
 
 
@@ -49,48 +47,48 @@ path = argv[0]
 
 ################################### Older version #################################
 
-import pandas as pd
-import sys
-from rdflib import Graph, Namespace, URIRef
-import os
-import sys
-from os import listdir
-from os.path import isfile, join
+# import pandas as pd
+# import sys
+# from rdflib import Graph, Namespace, URIRef
+# import os
+# import sys
+# from os import listdir
+# from os.path import isfile, join
 
-# Gets path as arguments
-argv = sys.argv[1:]
-path = argv[0]
+# # Gets path as arguments
+# argv = sys.argv[1:]
+# path = argv[0]
 
-def get_filename(path):
-    path_split = path.split(sep="/")
-    file = path_split[-1]
-    name_split = file.split(sep=".")
-    name = name_split[0]
-    filename = name + "." + "ttl"
-    return(filename)
+# def get_filename(path):
+#     path_split = path.split(sep="/")
+#     file = path_split[-1]
+#     name_split = file.split(sep=".")
+#     name = name_split[0]
+#     filename = name + "." + "ttl"
+#     return(filename)
 
-def get_path(path):
-    path_split = path.split(sep="/")
-    file = path_split[:-1]
-    root ='/'.join(file)
-    return(root)
-
-
-get1 = get_filename(path)
-get2 = get_path(path)
-
-g = Graph()
-g.parse(argv[0], format="nt")
-
-g.namespace_manager.bind('this', URIRef("https://w3id.org/duchenne-fdp/data/"))
-g.namespace_manager.bind('sio', URIRef("http://semanticscience.org/resource/"))
-g.namespace_manager.bind('obo', URIRef("http://purl.obolibrary.org/obo/"))
-all_ns = [n for n in g.namespace_manager.namespaces()]
-print(all_ns)
+# def get_path(path):
+#     path_split = path.split(sep="/")
+#     file = path_split[:-1]
+#     root ='/'.join(file)
+#     return(root)
 
 
-os.chdir(path=str(get2))
-g.serialize(destination= get1, format="turtle")
+# get1 = get_filename(path)
+# get2 = get_path(path)
+
+# g = Graph()
+# g.parse(argv[0], format="nt")
+
+# g.namespace_manager.bind('this', URIRef("https://w3id.org/duchenne-fdp/data/"))
+# g.namespace_manager.bind('sio', URIRef("http://semanticscience.org/resource/"))
+# g.namespace_manager.bind('obo', URIRef("http://purl.obolibrary.org/obo/"))
+# all_ns = [n for n in g.namespace_manager.namespaces()]
+# print(all_ns)
+
+
+# os.chdir(path=str(get2))
+# g.serialize(destination= get1, format="turtle")
 
 
 

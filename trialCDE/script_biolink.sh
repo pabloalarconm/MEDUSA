55e976eb6f68a35e1e1ab28b9c1d8702ce3e30f7
pabloalarconm <pabloalarconmoreno@gmail.com>
2022-02-08 15:49:46 +0100

    biolink commit

--- trialCDE/script_biolink.sh
+++ trialCDE/script_biolink.sh
@@ -3,8 +3,8 @@
 
 # sudo python3 uniq.py ${PWD}/data
 
-irb run_me_to_test.rb biolink_personal_information
-irb run_me_to_test.rb biolink_lab_measurement
+irb run_me_to_test.rb personal_information_biolink
+irb run_me_to_test.rb lab_measurement_biolink
 
 sudo python3 nt2ttl.py ${PWD}/data/triples
 
